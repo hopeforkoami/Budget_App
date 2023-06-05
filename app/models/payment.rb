0e1984a462a548291070e225c4eecd17824ce7c0
hopeforkoami <hopeforkoami@gmail.com>
2023-06-05 20:44:51 +0000

    Add validations

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -1,2 +1,7 @@
 class Payment < ApplicationRecord
+  belongs_to :group, class_name: 'Group', foreign_key: 'group_id'
+  belongs_to :user, class_name: 'User', foreign_key: 'user_id'
+
+  validates :name, presence: true, length:{in: 2..250 }
+  validates :amount, presence: true, numericality: { only_float: true, greater_than_or_equal_to: 0 }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+  has_many :groups, class_name: 'Group', foreign_key: 'user_id'
+
+  validates :name, presence: true, length: {in: 2..250 }
 end
