2e1ffc275a3a4bd79df3f7e21abcf98bc7b5cc7e
hopeforkoami <hopeforkoami@gmail.com>
2023-06-08 15:23:29 +0000

    set default homepage

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
   resources :not_found, only: [:index]
   # Defines the root path route ("/")
   # root "articles#index"
-  root 'hone#index'
+  root 'group#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
