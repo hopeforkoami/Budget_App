2f0f7100f752fd1b18a9d2b5c1c2db8044372d19
hopeforkoami <hopeforkoami@gmail.com>
2023-06-11 00:09:27 +0000

    Update files

--- app/controllers/splash_controller.rb
+++ app/controllers/splash_controller.rb
@@ -3,4 +3,10 @@ class SplashController < ApplicationController
   def index
     redirect_to group_index_path if user_signed_in?
   end
+
+  def logout
+    sign_out current_user
+    redirect_to splash_index_path
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
   
-  resources :splash, only: [:index]
+  resources :splash, only: [:index, :logout]
   resources :group do
     resources :payment
   end
 
   resources :not_found, only: [:index]
+  get 'logout', to: 'splash#logout'
   # Defines the root path route ("/")
   # root "articles#index"
   root 'splash#index'
