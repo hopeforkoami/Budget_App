58a58e628a624135718d1c6c0e4d6d964a2b91ab
hopeforkoami <hopeforkoami@gmail.com>
2023-06-11 05:43:38 +0000

    Fix many to many relationship

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_10_195217) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_11_053804) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -42,6 +42,11 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_10_195217) do
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
+  create_table "group_payments", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "groups", force: :cascade do |t|
     t.string "name"
     t.string "icon"
@@ -53,7 +58,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_10_195217) do
 
   create_table "payments", force: :cascade do |t|
     t.string "name"
-    t.decimal "amount"
+    t.integer "amount"
     t.bigint "user_id", null: false
     t.bigint "group_id", null: false
     t.datetime "created_at", null: false
